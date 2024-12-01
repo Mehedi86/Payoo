1e29c89a5a33a893203f01d24d123ce85cde183d
Mehedi86 <abdullahalmehedi@gmail.com>
2024-12-01 19:01:50 +0600

    navigation other page

--- js/login.js
+++ js/login.js
@@ -2,5 +2,13 @@ document.getElementById('login-btn').addEventListener('click', function(event){
     event.preventDefault();
     const phoneNumber = document.getElementById('number-field').value;
     const pinNumber = document.getElementById('pin-field').value;
+
+    if(phoneNumber === 'phone' && pinNumber === '1234'){
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('Wrong phone or pin')
+    }
+    console.log(phoneNumber, pinNumber)
     
 })
