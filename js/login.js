96c7f38fc79190a5638c550c6c668fafaccc18fb
Mehedi86 <abdullahalmehedi@gmail.com>
2024-12-01 18:36:13 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('number-field').value;
+    const pinNumber = document.getElementById('pin-field').value;
+    
+})
